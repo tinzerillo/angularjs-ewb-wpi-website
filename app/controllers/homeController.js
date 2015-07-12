bea1eb21d6796d3ce1901ac21ef3767b5c1a41e9
Theresa <tkinzerillo@wpi.edu>
2015-07-11 20:34:01 -0400

    JSLint fixes

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,11 +1,12 @@
-app.controller('homeController', function($scope) {
-        $scope.newsletters = [
-			{ "url":"May2014", "date":"May 2014"},
-			{ "url":"December2013", "date":"December 2013"},
-			{ "url":"November2013", "date":"November 2013"},
-			{ "url":"May2013", "date":"May 2013"},
-			{ "url":"April2012", "date":"April 2012"},
-			{ "url":"January2012", "date":"January 2012"},
-			{ "url":"December2010", "date":"December 2010"}
-		]	
-    }); 
+app.controller('homeController', function ($scope) {
+    "use strict";
+    $scope.newsletters = [
+        { "url": "May2014", "date": "May 2014"},
+        { "url": "December2013", "date": "December 2013"},
+        { "url": "November2013", "date": "November 2013"},
+        { "url": "May2013", "date": "May 2013"},
+        { "url": "April2012", "date": "April 2012"},
+        { "url": "January2012", "date": "January 2012"},
+        { "url": "December2010", "date": "December 2010"}
+    ];
+});
